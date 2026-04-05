f7426783329517bede14e0a0f32180c16b92a62c
deeppatel200590 <deepadroja964@gmail.com>
2026-04-05 10:57:22 +0530

    fix

--- backend/model/mailer.js
+++ backend/model/mailer.js
@@ -2,11 +2,12 @@ import nodemailer from "nodemailer";
 
 export const sendOTP = async (email, otp) => {
   try {
-    console.log("EMAIL:", process.env.EMAIL);
-    console.log("PASS:", process.env.EMAIL_PASS ? "EXISTS" : "MISSING");
+    console.log("Trying to send OTP...");
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // TLS
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
@@ -14,16 +15,16 @@ export const sendOTP = async (email, otp) => {
     });
 
     const info = await transporter.sendMail({
-      from: process.env.EMAIL,
+      from: `"Movie App" <${process.env.EMAIL}>`,
       to: email,
       subject: "OTP Verification",
       text: `Your OTP is ${otp}`
     });
 
-    console.log("EMAIL SENT:", info.response);
+    console.log("✅ EMAIL SENT:", info.response);
 
   } catch (error) {
-    console.log("❌ EMAIL ERROR FULL:", error);
+    console.log("❌ EMAIL ERROR:", error.message);
     throw new Error("Failed to send OTP");
   }
 };
