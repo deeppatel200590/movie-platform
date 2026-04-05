d9c41dc8e1f4cd2ebb044a4bc7befb87b78f9177
deeppatel200590 <deepadroja964@gmail.com>
2026-04-05 12:34:34 +0530

    fix login

--- backend/model/passport.js
+++ backend/model/passport.js
@@ -1,32 +1,40 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import User from "./Signup.js"; // ✅ correct path
+import User from "./Signup.js";
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ email: profile.emails[0].value });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({
+          email: profile.emails[0].value
+        });
 
-    if (!user) {
-      user = new User({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value
-      });
-    } else {
-      user.googleId = profile.id; // link account
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            isVerified: true,
+            role: "user"
+          });
+        } else {
+          user.googleId = profile.id;
+          user.isVerified = true;
+        }
+
+        await user.save();
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
     }
-
-    await user.save();
-    return done(null, user);
-
-  } catch (err) {
-    return done(err, null);
-  }
-}));
+  )
+);
 
 export default passport;
