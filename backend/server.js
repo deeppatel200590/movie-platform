21364e578d4aaba9684d07e8e8cc4052a0d859c2
deeppatel200590 <deepadroja964@gmail.com>
2026-04-17 00:29:41 +0530

    try2

--- backend/server.js
+++ backend/server.js
@@ -534,6 +534,8 @@ app.get("/auth/google/callback",
   }
 );
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
