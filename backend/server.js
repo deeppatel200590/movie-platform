b954642269f54d08ecede41ac1551dc8f498398a
deeppatel200590 <deepadroja964@gmail.com>
2026-04-16 23:32:16 +0530

    payment fix

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,10 @@ passport.deserializeUser(async (id, done) => {
 
 Cashfree.XClientId = process.env.CASHFREE_APP_ID;
 Cashfree.XClientSecret = process.env.CASHFREE_SECRET_KEY;
-Cashfree.XEnvironment = process.env.CASHFREE_ENV || "sandbox";
+Cashfree.XEnvironment =
+  process.env.CASHFREE_ENV === "production"
+    ? Cashfree.Environment.PRODUCTION
+    : Cashfree.Environment.SANDBOX;
 
 
 const auth = (req, res, next) => {
